c50225e1565903eed90a7bf371e1063ebe0c7ec3
Elysa Bouttier <elysa.bouttier@sbsr.fr>
2021-04-20 11:54:03 +0200

    urls bills added

--- app-server/management_app/bills/admin.py
+++ app-server/management_app/bills/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Bills
+from .models import BillsProduct
+
 
 # Register your models here.
+admin.site.register(Bills)
+admin.site.register(BillsProduct)
---
+++ app-server/management_app/bills/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+
+urlpatterns = [
+    # BILLS
+    # Create
+
+    # Read
+    path('bill/', views.BillList.as_view()),
+    path('bill/<int:pk>', views.BillDetails.as_view()),
+    # Update
+
+    # Delete
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
