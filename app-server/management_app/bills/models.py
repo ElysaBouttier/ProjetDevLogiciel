65274b9be4b3f7c236f5d2b050af02c1913f733a
Elysa Bouttier <elysa.bouttier@sbsr.fr>
2021-04-20 11:17:04 +0200

    add models in bills

--- app-server/management_app/bills/models.py
+++ app-server/management_app/bills/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
+from django.contrib.auth.models import Bills
+
 # Create your models here.
+
+class Bills(models.Model):
+    idClient = models.ForeignKey(Client, on_delete=models.CASCADE)
+    issuingDate = models.DateTimeField(auto_now_add=True)
+    isPaid = models.BooleanField(null=False)
+    payementDate = models.DateTimeField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    listeProductName = models.ForeignKey(BillsProduct, on_delete=models.CASCADE)
+
+
+class BillsProduct(models.Model):
+    idBills = models.ForeignKey(Bills, on_delete=models.CASCADE)
+    idProducts = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models..IntegerField(min_value=0, max_value=300)
