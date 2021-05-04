6340e62551c483907d8f3f83cf53dc73900a178c
Elysa Bouttier <elysa.bouttier@sbsr.fr>
2021-05-04 19:27:30 +0200

    issue on adding vuetify

--- app-frontend/manage-app/src/store/index.js
+++ app-frontend/manage-app/src/store/index.js
@@ -5,8 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    datas: [
+      { id: 5, name: 'Lala', lastname: 'teletobie', mail: 'lalal@valide.fr' },
+      { id: 1, name: 'Bid', lastname: 'Ul', mail: 'Bidul@valide.fr' },
+      { id: 2, name: 'Eve', lastname: 'A', mail: 'Eva@valide.fr' },
+      { id: 3, name: 'Flan', lastname: 'By', mail: 'mail@valide.fr' }
+    ]
   },
   mutations: {
+    ADD_ALERT (state, alert) {
+      state.alerts.push(alert)
+    },
+
+    UPDATE_IS_CONNECTED (state, isConnected) {
+      // mutate state
+      state.isConnected = isConnected
+    }
   },
   actions: {
   },
--- app-frontend/manage-app/src/views/About.vue
+++ app-frontend/manage-app/src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class='about'>
+    <template v-if='!showFormAddData'>
+      <Data :data='data' v-for='data in datas' :key='data.id' />
+    </template>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import { mapState } from 'vuex'
+import Data from '@/components/Data.vue'
+
+export default {
+  name: 'about',
+  props: {},
+
+  components: {
+    Data
+  },
+  data: () => ({
+    showFormAddData: false
+  }),
+  computed: {
+    ...mapState({
+      alerts: (state) => state.alerts,
+      datas: (state) => state.datas
+    })
+  },
+  methods: {}
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
