82f8adf0a668acbc69689ba025ef0e9431997eae
Elysa Bouttier <elysa.bouttier@sbsr.fr>
2021-05-10 11:55:04 +0200

    add new item dialog issue on create new item

--- app-frontend/app/src/views/Home.vue
+++ app-frontend/app/src/views/Home.vue
@@ -3,18 +3,88 @@
     <div>
       <p>This is the Home page</p>
     </div>
+
+    <!-- ---------------------------------------- -->
+    <!--  ---------------MODAL------------------- -->
+    <!-- ---------------------------------------- -->
+
+    <v-dialog
+      v-model="dialog"
+      max-width="500px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          class="mb-2"
+          v-bind="attrs"
+          @click="dialog = true"
+        >
+          New Item
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Add new {{renderModalTitle}}</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+              <v-text-field
+                :rules="rules"
+                hide-details="auto"
+              ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
+    <!-- ---------------------------------------- -->
+    <!--  ---------------SELECTOR---------------- -->
+    <!-- ---------------------------------------- -->
     <v-container fluid>
       <v-row align="center">
         <v-col class="d-flex" cols="12" sm="6">
           <v-select
             :items="items"
-            label="Faites votre choix"
+            label="Make a choice"
             v-model="choice"
           ></v-select>
         </v-col>
       </v-row>
     </v-container>
 
+
+    <!-- ---------------------------------------- -->
+    <!-- ----------------TABLE------------------- -->
+    <!-- ---------------------------------------- -->
     <v-simple-table
     fixed-header
     height="300px">
@@ -32,25 +102,74 @@ export default {
   props: {},
 
   data: () => ({
-    bills: [],
+    // ---------------MODAL-------------------
+    editedItem: {name: 'nom', calories: 'calorie'},
+    dialog: false,
+    rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+
+    // ---------------SELECTOR----------------
     selectedItem: 1,
+    choice: null,
+
+    // ----------------TABLE------------------
+    requestAPI: [],
     items: [
       { text: "Products", icon: "mdi-clock", value: "PRODUCTS" },
       { text: "Clients", icon: "mdi-account", value: "CLIENTS" },
       { text: "Bills", icon: "mdi-flag", value: "BILLS" },
     ],
-
-    choice: null,
   }),
+
   computed: {
+    // ---------------MODAL-------------------
+
+    renderModalTitle(){
+      if(this.requestAPI){
+        if(this.choice == "PRODUCTS"){
+          return "product"
+        }
+        if(this.choice == "CLIENTS"){
+          return "client"
+        }
+        if(this.choice == "BILLS"){
+          return "bill"
+        }
+        else{
+          return ""
+        }
+      }
+      else{
+        return ' ';
+      }
+    },
+    renderModalImput(){
+      if(this.requestAPI){
+        return null
+      }
+      else{
+        return ' '
+      }
+    },
+    // },
+    // validateModalImput(){
+
+    // },
+
+    // ---------------SELECTOR----------------
+    
+    // ----------------TABLE------------------
+    // Show data inside the table  READ of CRUD
     renderTableData() {
-      if (!this.bills.length) {
+      if (!this.requestAPI.length) {
         return;
       }
 
-      let objectKeys = Object.keys(this.bills[0]);
+      let objectKeys = Object.keys(this.requestAPI[0]);
 
-      return this.bills
+      return this.requestAPI
         .map((bill) => {
           const body = [];
 
@@ -66,11 +185,11 @@ export default {
     },
 
     renderTableHeader() {
-      if (!this.bills.length) {
+      if (!this.requestAPI.length) {
         return;
       }
-      console.log(this.bills[0]);
-      let header = Object.keys(this.bills[0]);
+      // console.log(this.requestAPI[0]);
+      let header = Object.keys(this.requestAPI[0]);
       return header
         .map((key, index) => {
           return `<th key=${index}>${key.toUpperCase()}</th>`;
@@ -78,6 +197,14 @@ export default {
         .join()
         .replace(/,/g, "");
     },
+
+// Modal windows
+    // close () {
+    //   this.dialog = false
+    // },
+    // save () {
+    //   return this.close()
+    // },
   },
 
   watch: {
@@ -100,7 +227,7 @@ export default {
         const url = `http://127.0.0.1:8000/${resource}/`;
         const { data } = await axios.get(url);
 
-        this.bills = data;
+        this.requestAPI = data;
       } catch (e) {
         console.error(e);
       }
